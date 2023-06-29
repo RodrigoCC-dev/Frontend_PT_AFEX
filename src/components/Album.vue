76706e90f19533338072131cb057bb734f740759
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-06-29 01:02:08 -0400

    Form: Se agrega 'input' lectura de enlace, verificaciones asociadas y obtención de información desde 'Youtube API'

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,4 +1,41 @@
 <script setup>
+import axios from 'axios'
+
+let link = ''
+let videoData = {}
+
+function getVideoId() {
+  let videoId = ''
+  if (link.includes('v=')) {
+    videoId = link.split('v=')[1]
+  } else {
+    let aux = link.split('/')
+    videoId = aux[aux.length -1]
+  }
+  return videoId
+}
+
+function isValidUrl() {
+  let aux = link.split('/')
+  return aux.includes('www.youtube.com') || aux.includes('youtu.be')
+}
+
+async function getInfo() {
+  if (isValidUrl()) {
+    let videoId = getVideoId()
+    let url = 'https://www.googleapis.com/youtube/v3/videos?id=' + videoId + '&key=' + import.meta.env.VITE_API_KEY + '&part=snippet%2CcontentDetails'
+    try {
+      const response = await axios.get(url)
+      videoData = response.data
+      console.log(videoData)
+    } catch (e) {
+      console.log('Hubo un error al obtener la data')
+    }
+  } else {
+    console.log('Dirección no es enlace de youtube válido')
+  }
+}
+
 </script>
 
 <template>
@@ -9,10 +46,10 @@
       </div>
       <div class="field has-addons">
         <div class="control is-expanded">
-          <input class="input" type="text" placeholder="Enlace youtube">
+          <input class="input" type="text" placeholder="Enlace youtube" v-model="link">
         </div>
         <div class="control">
-          <a class="button px-6 is-info">Añadir</a>
+          <a class="button px-6 is-info" @click="getInfo">Añadir</a>
         </div>
       </div>
     </div>
