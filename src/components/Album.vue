aeb85600a677fbbb2868aabb438e5de80d7c6495
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-06-28 21:59:38 -0400

    App: Se configuran estilos y apariencia inicial

---
+++ src/components/Album.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="mt-5">
+      <div class="content">
+        <h2>Añadir nuevo video</h2>
+      </div>
+      <div class="field has-addons">
+        <div class="control is-expanded">
+          <input class="input" type="text" placeholder="Enlace youtube">
+        </div>
+        <div class="control">
+          <a class="button px-6 is-info">Añadir</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
