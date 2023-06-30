ffa0bfdcf1a559e104e60097c56701bf1588f4c8
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-06-29 21:03:37 -0400

    Lista: Se configura posición de botón 'delete' y tiempo de duración en los items de la lista

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -6,6 +6,14 @@ const props = defineProps(['item'])
 
 <template>
   <div class="column is-one-third has-text-centered my-5">
-    <img :src="props.item.thumbUrl" :alt="props.item.title">
+    <div class="new-container">
+      <img :src="props.item.thumbUrl" :alt="props.item.title">
+      <button class="new-delete top-right"></button>
+      <p class="duration-text bottom-right">{{item.duration}}</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+@import '../assets/items.css';
+</style>
